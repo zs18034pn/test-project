871a97053420a7acd7222b7b2bc49ed626e9de53
zs18034pn <zoja.scekic@gmail.com>
2020-03-02 19:47:42 +0100

    Nedelja 5

--- Nedelja 5/JavaScript3/index.js
+++ Nedelja 5/JavaScript3/index.js
@@ -2,7 +2,7 @@ function buttonClicked(e) {
     console.log(e);
     console.log(e.target);
     console.log(e.target.id);
-    
+
     const header = document.getElementById("header-title");
     header.textContent = "Changed";
     header.style.color = "black";
@@ -10,3 +10,4 @@ function buttonClicked(e) {
 
 const button = document.querySelector("#button");
 button.addEventListener("click", buttonClicked);
+{once: true}
